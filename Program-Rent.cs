0fd4dc7fb974fd3efb4aaf5a924b66e3d00bbad7
fenrirfx <83608537+fenrirfx@users.noreply.github.com>
2021-07-01 19:16:00 +0100

    Add files via upload

---
+++ Program-Rent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o número de quartos a alugar:");
+            int n = int.Parse(Console.ReadLine());
+
+            Rent[] vect = new Rent[10];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Rent#" + (i + 1));
+                Console.Write("Insira o Nome:");
+                string nome = Console.ReadLine();
+                Console.Write("Insira o Email:");
+                string email = Console.ReadLine();
+                Console.Write("Número do Quarto:");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Rent { Nome = nome, Email = email, NumeroQuarto = quarto };
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                    Console.WriteLine(vect[i]);
+            }
+        }
+        
+    }
+}
---
+++ Rent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel
+{
+    class Rent
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int NumeroQuarto { get; set; }
+
+        public override string ToString()
+        {
+            return NumeroQuarto+": "
+            + Nome
+            + ", "
+            + Email;
+        }
+    }
+}
